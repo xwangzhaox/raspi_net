9474ffbbe7e59cd708353e105be346ee7b8ef47c
Kevin.wang <xwangzhaox@126.com>
2017-05-18 10:44:04 +0800

    new change for server1 and node1 code

--- nodes/node_th_and_p/node_th_and_p.ino
+++ nodes/node_th_and_p/node_th_and_p.ino
@@ -8,28 +8,37 @@
 #include <SPI.h>
 #include "RF24.h"
 
-/****************** User Config ***************************/
-/***      Set this radio as radio number 0 or 1         ***/
-bool radioNumber = 0;
+const enum commands {pOn, pOff, getAirTH};
 
-/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+struct payload_request_t{
+  char message[15];
+};
+
+struct payload_general_t{
+  enum command;
+  int destination;
+  char message[14];
+};
+
+payload_request_t outgoing;
+payload_general_t incoming;
+
 RF24 radio(7,8);
-/**********************************************************/
 
 void setup() {
   Serial.begin(115200);
-  Serial.println(F("RF24/examples/GettingStarted"));
-  
+  Serial.println(F("Arduino start listening..."));
+
   radio.begin();
 
   // Set the PA Level low to prevent power supply related issues since this is a
  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
   radio.setPALevel(RF24_PA_LOW);
-  
+
   // Open a writing and reading pipe on each radio, with opposite addresses
-  radio.openWritingPipe("arduino1");
-  radio.openReadingPipe(1,"server1");
-  
+  radio.openWritingPipe("Node1");
+  radio.openReadingPipe(1,"Server1");
+
   // Start the radio listening for data
   radio.startListening();
 }
@@ -37,33 +46,29 @@ void setup() {
 void loop() {
 /****************** Pong Back Role ***************************/
 
-  if ( role == 0 )
-  {
     unsigned long got_time;
-    
+
     if( radio.available()){
                                                                     // Variable for the received timestamp
       while (radio.available()) {                                   // While there is data ready
         // get command data
-        radio.read( data, sizeof(data) );             // Get the payload
+        radio.read(&incoming, sizeof(payload_general_t));
       }
 
-      if(data.command=="getTH"){
-        // Get T and H
-        // Send JSON data back.
-        
-      }elsif(data.command=="p_on_2"){
-        // port * power on
-      }elsif(data.command=="p_off_2"){
-        // port * power off
+      if(data.command==pOn){
+        Serial.print(F("Power on"));
+      }elsif(data.command==pOff){
+        Serial.print(F("Power off"));
+      }elsif(data.command==getAirTH){
+        Serial.print(F("Air TH"));
       }
 
-      radio.stopListening();                                        // First, stop listening so we can talk   
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
+      radio.stopListening();                                        // First, stop listening so we can talk
+      outgoing.message = "OK";
+      radio.write( &outgoing, sizeof(payload_request_t) );              // Send the final one back.
+      radio.startListening();                                       // Now, resume listening so we catch the next packets.
       Serial.print(F("Sent response "));
-      Serial.println(got_time);  
+      Serial.println(outgoing.message);
    }
- }
 } // Loop
 
